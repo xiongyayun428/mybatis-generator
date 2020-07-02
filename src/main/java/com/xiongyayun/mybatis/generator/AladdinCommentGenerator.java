19572256d04a78624d9933144a02b394ccfbef29
熊亚运 <xiongyayun428@163.com>
2020-07-02 18:57:35 +0800

    init

---
+++ src/main/java/com/xiongyayun/mybatis/generator/AladdinCommentGenerator.java
@@ -0,0 +1,250 @@
+package com.xiongyayun.mybatis.generator;
+
+import org.mybatis.generator.api.CommentGenerator;
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.dom.xml.XmlElement;
+import org.mybatis.generator.config.MergeConstants;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * AladdinCommentGenerator
+ *
+ * @author: <a href="mailto:xiongyayun428@163.com">Yayun.Xiong</a>
+ * @date: 2020/6/28
+ */
+public class AladdinCommentGenerator implements CommentGenerator {
+    private final Properties properties;
+    private Properties systemPro;
+    /**
+     * 父类时间
+     */
+    private boolean suppressDate;
+
+    private String superInterface;
+
+    private String author;
+    private String dateFormat;
+
+    public AladdinCommentGenerator() {
+        properties = new Properties();
+        systemPro = System.getProperties();
+        suppressDate = false;
+    }
+
+    @Override
+    public void addConfigurationProperties(Properties properties) {
+        // 获取自定义的 properties
+        this.properties.putAll(properties);
+        this.superInterface = properties.getProperty("superInterface");
+        this.author = properties.getProperty("author", systemPro.getProperty("user.name"));
+        this.dateFormat = properties.getProperty("dateFormat", "yyyy-MM-dd");
+    }
+
+    private boolean isEmpty(String obj) {
+        if (obj == null || "".equals(obj)) {
+            return true;
+        }
+        return false;
+    }
+
+    private String getCurrentDate() {
+        return (new SimpleDateFormat(dateFormat)).format(new Date());
+    }
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        // 获取列注释
+        String remarks = introspectedColumn.getRemarks();
+        field.addJavaDocLine("/**");
+        field.addJavaDocLine(" * " + ((remarks == null || "".equals(remarks)) ? introspectedColumn.getActualColumnName() : remarks));
+        field.addJavaDocLine(" */");
+    }
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable) {
+        field.addJavaDocLine("/**");
+        field.addJavaDocLine(" * " + introspectedTable.getFullyQualifiedTable());
+        field.addJavaDocLine(" */");
+    }
+
+    @Override
+    public void addModelClassComment(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        if (superInterface != null && !"".equals(superInterface)) {
+            topLevelClass.addImportedType(superInterface);
+            topLevelClass.addSuperInterface(new FullyQualifiedJavaType(superInterface.substring(superInterface.lastIndexOf(".")+1,superInterface.length())));
+        }
+
+        // 获取表注释
+        String remarks = introspectedTable.getRemarks();
+
+        topLevelClass.addJavaDocLine("/**");
+        topLevelClass.addJavaDocLine(" * " + remarks);
+        topLevelClass.addJavaDocLine(" *");
+        topLevelClass.addJavaDocLine(" * @author " + author);
+        topLevelClass.addJavaDocLine(" * @date   " + getCurrentDate());
+        topLevelClass.addJavaDocLine(" */");
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable) {
+        StringBuilder sb = new StringBuilder();
+        innerClass.addJavaDocLine("/**");
+        sb.append(" * ");
+        sb.append(introspectedTable.getFullyQualifiedTable());
+        sb.append(" ");
+        sb.append(getDateString());
+        innerClass.addJavaDocLine(sb.toString().replace("\n", " "));
+        innerClass.addJavaDocLine(" */");
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable, boolean b) {
+        StringBuilder sb = new StringBuilder();
+        innerClass.addJavaDocLine("/**");
+        sb.append(" * ");
+        sb.append(" * ");
+        sb.append(introspectedTable.getFullyQualifiedTable());
+        innerClass.addJavaDocLine(sb.toString().replace("\n", " "));
+        sb.setLength(0);
+        sb.append(" * @author ");
+        sb.append(systemPro.getProperty("user.name"));
+        sb.append(" ");
+        sb.append(getCurrentDate());
+        innerClass.addJavaDocLine(" */");
+    }
+
+    @Override
+    public void addEnumComment(InnerEnum innerEnum, IntrospectedTable introspectedTable) {
+        innerEnum.addJavaDocLine("/**");
+        innerEnum.addJavaDocLine(" * " + introspectedTable.getFullyQualifiedTable());
+        innerEnum.addJavaDocLine(" */");
+    }
+
+    @Override
+    public void addGetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        method.addJavaDocLine("/**");
+        StringBuilder sb = new StringBuilder();
+        sb.append(" * ");
+        sb.append(introspectedColumn.getRemarks());
+        method.addJavaDocLine(sb.toString().replace("\n", " "));
+        sb.setLength(0);
+        sb.append(" * @return ");
+        sb.append(introspectedColumn.getActualColumnName());
+        sb.append(" ");
+        sb.append(introspectedColumn.getRemarks());
+        method.addJavaDocLine(sb.toString().replace("\n", " "));
+        method.addJavaDocLine(" */");
+    }
+
+    @Override
+    public void addSetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        method.addJavaDocLine("/**");
+        StringBuilder sb = new StringBuilder();
+        sb.append(" * ");
+        sb.append(introspectedColumn.getRemarks());
+        method.addJavaDocLine(sb.toString().replace("\n", " "));
+        Parameter parameter = method.getParameters().get(0);
+        sb.setLength(0);
+        sb.append(" * @param ");
+        sb.append(parameter.getName());
+        sb.append(" ");
+        sb.append(introspectedColumn.getRemarks());
+        method.addJavaDocLine(sb.toString().replace("\n", " "));
+        method.addJavaDocLine(" */");
+    }
+
+    @Override
+    public void addGeneralMethodComment(Method method, IntrospectedTable introspectedTable) {
+        method.addJavaDocLine("/**");
+        addJavadocTag(method, false);
+        method.addJavaDocLine(" */");
+    }
+
+    @Override
+    public void addJavaFileComment(CompilationUnit compilationUnit) {
+
+    }
+
+    @Override
+    public void addComment(XmlElement xmlElement) {
+
+    }
+
+    @Override
+    public void addRootComment(XmlElement xmlElement) {
+
+    }
+
+    @Override
+    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    @Override
+    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    @Override
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    @Override
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    @Override
+    public void addClassAnnotation(InnerClass innerClass, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    /**
+     *
+     *@Title getDateString
+     *@Description: 此方法返回格式化的日期字符串以包含在Javadoc标记中和XML注释。 如果您不想要日期，则可以返回null在这些文档元素中。
+     *@Author fendo
+     *@Date 2017年10月5日 下午3:45:58
+     *@return
+     *@throws
+     */
+    protected String getDateString() {
+        String result = null;
+        if (!suppressDate) {
+            result = getCurrentDate();
+        }
+        return result;
+    }
+    /**
+     *
+     *@Title addJavadocTag
+     *@Description: 此方法为其添加了自定义javadoc标签。
+     *@Author fendo
+     *@Date 2017年10月5日 下午3:49:05
+     *@param javaElement
+     *@param markAsDoNotDelete
+     *@throws
+     */
+    protected void addJavadocTag(JavaElement javaElement, boolean markAsDoNotDelete) {
+        javaElement.addJavaDocLine(" *");
+        StringBuilder sb = new StringBuilder();
+        sb.append(" * ");
+        sb.append(MergeConstants.NEW_ELEMENT_TAG);
+        if (markAsDoNotDelete) {
+            sb.append(" do_not_delete_during_merge");
+        }
+        String s = getDateString();
+        if (s != null) {
+            sb.append(' ');
+            sb.append(s);
+        }
+        javaElement.addJavaDocLine(sb.toString());
+    }
+}
