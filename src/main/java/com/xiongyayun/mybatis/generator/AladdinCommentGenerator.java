c52bbc40f1c90f6efdd6233f61b7a9512acafb02
熊亚运 <xiongyayun428@163.com>
2020-07-03 18:17:20 +0800

    init

--- src/main/java/com/xiongyayun/mybatis/generator/AladdinCommentGenerator.java
+++ src/main/java/com/xiongyayun/mybatis/generator/AladdinCommentGenerator.java
@@ -5,10 +5,10 @@ import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.dom.java.*;
 import org.mybatis.generator.api.dom.xml.XmlElement;
-import org.mybatis.generator.config.MergeConstants;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Properties;
 import java.util.Set;
 
@@ -62,15 +62,26 @@ public class AladdinCommentGenerator implements CommentGenerator {
         // 获取列注释
         String remarks = introspectedColumn.getRemarks();
         field.addJavaDocLine("/**");
-        field.addJavaDocLine(" * " + ((remarks == null || "".equals(remarks)) ? introspectedColumn.getActualColumnName() : remarks));
+        field.addJavaDocLine(" * " + (isEmpty(remarks) ? introspectedColumn.getActualColumnName() : remarks));
         field.addJavaDocLine(" */");
     }
 
     @Override
     public void addFieldComment(Field field, IntrospectedTable introspectedTable) {
-        field.addJavaDocLine("/**");
-        field.addJavaDocLine(" * " + introspectedTable.getFullyQualifiedTable());
-        field.addJavaDocLine(" */");
+        StringBuilder sb = new StringBuilder();
+        sb.append("/** ");
+        String fieldName = field.getName();
+        if ("orderByClause".equals(fieldName)) {
+            sb.append(" 排序字段");
+        } else if ("distinct".equals(fieldName)) {
+            sb.append(" 过滤重复数据");
+        } else if ("oredCriteria".equals(fieldName)) {
+            sb.append(" 当前的查询条件实例");
+        } else if ("isDistinct".equals(fieldName)) {
+            sb.append(" 是否过滤重复数据");
+        }
+        sb.append(" */");
+        field.addJavaDocLine(sb.toString());
     }
 
     @Override
@@ -85,6 +96,7 @@ public class AladdinCommentGenerator implements CommentGenerator {
 
         topLevelClass.addJavaDocLine("/**");
         topLevelClass.addJavaDocLine(" * " + remarks);
+        topLevelClass.addJavaDocLine(" * " + introspectedTable.getFullyQualifiedTable());
         topLevelClass.addJavaDocLine(" *");
         topLevelClass.addJavaDocLine(" * @author " + author);
         topLevelClass.addJavaDocLine(" * @date   " + getCurrentDate());
@@ -131,13 +143,20 @@ public class AladdinCommentGenerator implements CommentGenerator {
         method.addJavaDocLine("/**");
         StringBuilder sb = new StringBuilder();
         sb.append(" * ");
-        sb.append(introspectedColumn.getRemarks());
+        String remarks = introspectedColumn.getRemarks();
+        if (isEmpty(remarks)) {
+            sb.append(introspectedColumn.getActualColumnName());
+        } else {
+            sb.append("获取" + remarks);
+        }
         method.addJavaDocLine(sb.toString().replace("\n", " "));
         sb.setLength(0);
         sb.append(" * @return ");
         sb.append(introspectedColumn.getActualColumnName());
-        sb.append(" ");
-        sb.append(introspectedColumn.getRemarks());
+        if (!isEmpty(remarks)) {
+            sb.append(" - ");
+            sb.append(remarks);
+        }
         method.addJavaDocLine(sb.toString().replace("\n", " "));
         method.addJavaDocLine(" */");
     }
@@ -147,23 +166,102 @@ public class AladdinCommentGenerator implements CommentGenerator {
         method.addJavaDocLine("/**");
         StringBuilder sb = new StringBuilder();
         sb.append(" * ");
-        sb.append(introspectedColumn.getRemarks());
+        String remarks = introspectedColumn.getRemarks();
+        if (isEmpty(remarks)) {
+            sb.append(introspectedColumn.getActualColumnName());
+        } else {
+            sb.append("设置" + remarks);
+        }
         method.addJavaDocLine(sb.toString().replace("\n", " "));
         Parameter parameter = method.getParameters().get(0);
         sb.setLength(0);
         sb.append(" * @param ");
         sb.append(parameter.getName());
-        sb.append(" ");
-        sb.append(introspectedColumn.getRemarks());
+        if (!isEmpty(remarks)) {
+            sb.append(" ");
+            sb.append(remarks);
+        }
         method.addJavaDocLine(sb.toString().replace("\n", " "));
         method.addJavaDocLine(" */");
     }
 
     @Override
     public void addGeneralMethodComment(Method method, IntrospectedTable introspectedTable) {
-//        method.addJavaDocLine("/**");
-//        addJavadocTag(method, false);
-//        method.addJavaDocLine(" */");
+        StringBuilder sb = new StringBuilder();
+        method.addJavaDocLine("/**");
+        sb.append(" * ");
+        if (method.isConstructor()) {
+            sb.append("构造查询条件");
+        }
+
+        final List<Parameter> parameterList = method.getParameters();
+        String methodName = method.getName();
+        if ("setOrderByClause".equals(methodName)) {
+            sb.append("设置排序字段");
+        } else if ("setDistinct".equals(methodName)) {
+            sb.append("设置过滤重复数据");
+        } else if ("getOredCriteria".equals(methodName)) {
+            sb.append("获取当前的查询条件实例");
+        } else if ("isDistinct".equals(methodName)) {
+            sb.append("是否过滤重复数据");
+        } else if ("getOrderByClause".equals(methodName)) {
+            sb.append("获取排序字段");
+        } else if ("createCriteria".equals(methodName)) {
+            sb.append("创建一个查询条件");
+        } else if ("createCriteriaInternal".equals(methodName)) {
+            sb.append("内部构建查询条件对象");
+        } else if ("clear".equals(methodName)) {
+            sb.append("清除查询条件");
+        } else if ("countByExample".equals(methodName)) {
+            sb.append("根据指定的条件获取数据库记录数");
+        } else if ("deleteByExample".equals(methodName)) {
+            sb.append("根据指定的条件删除数据库符合条件的记录");
+        } else if ("deleteByPrimaryKey".equals(methodName)) {
+            sb.append("根据主键删除数据库的记录");
+        } else if ("insert".equals(methodName)) {
+            sb.append("新写入数据库记录");
+        } else if ("insertSelective".equals(methodName)) {
+            sb.append("动态字段,写入数据库记录");
+        } else if ("selectByExample".equals(methodName)) {
+            sb.append("根据指定的条件查询符合条件的数据库记录");
+        } else if ("selectByPrimaryKey".equals(methodName)) {
+            sb.append("根据指定主键获取一条数据库记录");
+        } else if ("updateByExampleSelective".equals(methodName)) {
+            sb.append("动态根据指定的条件来更新符合条件的数据库记录");
+        } else if ("updateByExample".equals(methodName)) {
+            sb.append("根据指定的条件来更新符合条件的数据库记录");
+        } else if ("updateByPrimaryKeySelective".equals(methodName)) {
+            sb.append("动态字段,根据主键来更新符合条件的数据库记录");
+        } else if ("updateByPrimaryKey".equals(methodName)) {
+            sb.append("根据主键来更新符合条件的数据库记录");
+        } else if ("or".equals(methodName)) {
+            if (parameterList.isEmpty()) {
+                sb.append("创建一个新的或者查询条件");
+            } else {
+                sb.append("增加或者的查询条件,用于构建或者查询");
+            }
+        }
+//        sb.append(",");
+//        sb.append(introspectedTable.getFullyQualifiedTable());
+        method.addJavaDocLine(sb.toString());
+
+        String parameterName;
+        for (Parameter parameter : parameterList) {
+            sb.setLength(0);
+            sb.append(" * @param ");
+            parameterName = parameter.getName();
+            sb.append(parameterName);
+            if ("orderByClause".equals(parameterName)) {
+                sb.append(" 排序字段");
+            } else if ("distinct".equals(parameterName)) {
+                sb.append(" 是否过滤重复数据");
+            } else if ("criteria".equals(parameterName)) {
+                sb.append(" 过滤条件实例");
+            }
+            method.addJavaDocLine(sb.toString());
+        }
+        method.addJavaDocLine(" * @return " + method.getReturnType().get());
+        method.addJavaDocLine(" */");
     }
 
     @Override
@@ -206,45 +304,11 @@ public class AladdinCommentGenerator implements CommentGenerator {
 
     }
 
-    /**
-     *
-     *@Title getDateString
-     *@Description: 此方法返回格式化的日期字符串以包含在Javadoc标记中和XML注释。 如果您不想要日期，则可以返回null在这些文档元素中。
-     *@Author fendo
-     *@Date 2017年10月5日 下午3:45:58
-     *@return
-     *@throws
-     */
     protected String getDateString() {
         String result = null;
         if (!suppressDate) {
             result = getCurrentDate();
         }
         return result;
-    }
-    /**
-     *
-     *@Title addJavadocTag
-     *@Description: 此方法为其添加了自定义javadoc标签。
-     *@Author fendo
-     *@Date 2017年10月5日 下午3:49:05
-     *@param javaElement
-     *@param markAsDoNotDelete
-     *@throws
-     */
-    protected void addJavadocTag(JavaElement javaElement, boolean markAsDoNotDelete) {
-        javaElement.addJavaDocLine(" *");
-        StringBuilder sb = new StringBuilder();
-        sb.append(" * ");
-        sb.append(MergeConstants.NEW_ELEMENT_TAG);
-        if (markAsDoNotDelete) {
-            sb.append(" do_not_delete_during_merge");
-        }
-        String s = getDateString();
-        if (s != null) {
-            sb.append(' ');
-            sb.append(s);
-        }
-        javaElement.addJavaDocLine(sb.toString());
     }
 }
---
+++ src/main/java/com/xiongyayun/mybatis/generator/MybatisGenerator.java
@@ -0,0 +1,10 @@
+package com.xiongyayun.mybatis.generator;
+
+/**
+ * MybatisGenerator
+ *
+ * @author: <a href="mailto:xiongyayun428@163.com">Yayun.Xiong</a>
+ * @date: 2020/7/2
+ */
+public class MybatisGenerator {
+}
